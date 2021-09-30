c424e3c69aa29736ddc12b07aac5245f3cf858b5
Nishant Jha <me@nishantjha.org>
2021-09-29 20:32:30 -0400

    Hello World

--- landing-page/src/App.tsx
+++ landing-page/src/App.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <h1> Hello World </h1>
-    </div>
+    <Container>
+      <NavBar />
+      <Hero />
+    </Container>
   );
-}
+};
+
+const Hero = () => <Container>Hero Image!</Container>;
+const NavBar = () => <Container>Nav Bar!</Container>;
 
 export default App;
