d162512795679c9335821c7d124a35b3e3d670f6
Nishant Jha <me@nishantjha.org>
2021-09-29 21:53:53 -0400

    Hero and NavBar init

--- landing-page/src/App.tsx
+++ landing-page/src/App.tsx
@@ -1,18 +1,41 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container } from "react-bootstrap";
+import { Container, Image, Navbar } from "react-bootstrap";
+import dummyHero from "./images/dummy_hero.jpg";
 
 const App = () => {
+  const appStyle = {};
   return (
-    <Container>
+    <div style={appStyle}>
       <NavBar />
       <Hero />
-    </Container>
+    </div>
   );
 };
 
-const Hero = () => <Container>Hero Image!</Container>;
-const NavBar = () => <Container>Nav Bar!</Container>;
+const Hero = () => {
+  return (
+    <Container
+      fluid
+      style={{
+        backgroundImage: `url(${dummyHero})`,
+        backgroundSize: "contain",
+        backgroundRepeat: "no-repeat",
+        width: "100%",
+        height: "0",
+        paddingTop: "70%",
+      }}
+    ></Container>
+  );
+};
+
+const NavBar = () => (
+  <Navbar bg="dark" variant="dark">
+    <Container>
+      <Navbar.Brand href="#home">Attest</Navbar.Brand>
+    </Container>
+  </Navbar>
+);
 
 export default App;
